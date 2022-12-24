2292286b35f0dea438869c2ec8e58a108fd528a2
Mehdi Cherti <m.cherti@fz-juelich.de>
2022-12-24 19:38:09 +0100

    script to eval all

---
+++ eval_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for model in ddgan_sd_v10 ddgan_laion2b_v2 ddgan_ddb_v1 ddgan_ddb_v2 ddgan_ddb_v3;do 
+    if [ "$model" == "$ddgan_ddb_v3" ]; then
+        bs=32
+    else
+        bs=64
+    fi
+    sbatch --partition dc-gpu -t 360 -N 1 -n1 scripts/run_jurecadc_ddp.sh run.py test $model --cond-text=parti_prompts.txt  --batch-size=$bs --epoch=-1 --compute-clip-score --eval-name=parti;
+    sbatch --partition dc-gpu -t 360 -N 1 -n1 scripts/run_jurecadc_ddp.sh run.py test $model --fid --real-img-dir inception_statistics_coco_val2014_256x256.npz --cond-text coco_val2014_captions.txt  --batch-size=$bs --epoch=-1 --nb-images-for-fid=30000 --eval-name=coco --compute-clip-score;
+    sbatch --partition dc-gpu -t 360 -N 1 -n1 scripts/run_jurecadc_ddp.sh run.py test $model --cond-text=drawbench.txt  --batch-size=$bs --epoch=-1 --compute-clip-score --eval-name=drawbench;
+done
